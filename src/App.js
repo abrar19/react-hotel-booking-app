aab0112af14de69255672e8c5c0503a3e2bcc3b2
Abrar Haque <abm.abrar@gmail.com>
2023-03-17 05:49:18 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./app.css";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
 import Home from "./pages/home/Home";
